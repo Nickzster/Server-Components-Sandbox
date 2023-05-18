f0851fbaad3c7d12b09887c8b08329784cd4c90e
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-05-17 19:50:33 -0700

    changes

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -20,11 +20,11 @@ const Sandbox = () => {
   return (
     <div className="p-10">
       <h1 className="text-xl font-bold">Sandbox</h1>
-      <div className="flex">
-        <TodoList items={items} />
-      </div>
-      <br />
       <Client />
+      <br />
+      <div className="flex">
+        <TodoList items={items.reverse()} />
+      </div>
     </div>
   );
 };
---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log("Middleware!!");
+  request.headers.set("Foo", "Bar");
+}
