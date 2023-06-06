c8eed5d71475f6a91aa6e970502e87e466e13ba8
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-06-05 18:58:43 -0700

    Update sandbox 2 to support sessions

--- src/app/sandbox2/page.tsx
+++ src/app/sandbox2/page.tsx
@@ -2,33 +2,42 @@ import React from "react";
 import Client from "./client";
 import InMemoryDB from "../db";
 
-const MY_SUPER_DUPER_SECRET_VALUE_THAT_SHOULD_NOT_BE_LEAKED = "secretsauce";
+const A_SECRET = "secretsauce";
 
-const operationThatUsesSecret = async (text: string) => {
-  InMemoryDB.connect().addToDb(text);
-  console.log(MY_SUPER_DUPER_SECRET_VALUE_THAT_SHOULD_NOT_BE_LEAKED);
-  await fetch("https://www.google.com");
+const operationThatUsesSecret = async (text: string, session?: string) => {
+  if (!session) return;
+  InMemoryDB.connect().addToDb(text, session);
+  console.log(A_SECRET);
 };
 
-const Sandbox = () => {
-  const items: string[] = InMemoryDB.connect().readDb();
+const Sandbox = ({ searchParams }: { searchParams: { id?: string } }) => {
+  let sessionId = searchParams.id;
+
+  if (!sessionId)
+    return (
+      <h1 className="p-10 text-lg font-bold">
+        Invalid session! Append an <code>?id=YOUR_NAME</code> to the URL to
+        proceed.
+      </h1>
+    );
+
+  const items = InMemoryDB.connect().getSessionItems(sessionId);
 
   // BETTER: Write sensitive operations as part of a different closure + reference it.
   const addTodoItem = async (text: string) => {
     "use server";
-    await operationThatUsesSecret(text);
-    return InMemoryDB.connect().readDb();
+    await operationThatUsesSecret(text, sessionId);
+    console.log(sessionId);
+    return InMemoryDB.connect().getSessionItems(sessionId);
   };
 
   // BAD: This will leak the secret to the client via the network tab:
   // const addTodoItem = async (text: string) => {
   //   "use server";
   //   InMemoryDB.connect().addToDb(text);
-  //   console.log(MY_SUPER_DUPER_SECRET_VALUE_THAT_SHOULD_NOT_BE_LEAKED);
+  //   console.log(A_SECRET);
   //   return InMemoryDB.connect().readDb();
   // };
-
-  console.log("Returning items", items);
 
   return (
     <div className="p-10">
